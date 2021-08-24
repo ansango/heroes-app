70afa16442ca51afa0eef619a8f4e5ee1ee8e46a
ansango <anibalsantosgo@gmail.com>
2021-08-24 19:07:23 +0200

    route push

--- src/components/heroes/HeroScreen.js
+++ src/components/heroes/HeroScreen.js
@@ -1,17 +1,43 @@
 import { Redirect, useParams } from "react-router-dom";
 import { getHeroesById } from "../../selectors/getHeroesById";
-export const HeroScreen = () => {
+export const HeroScreen = ({ history }) => {
   const { heroId } = useParams();
-  const hero = getHeroesById(heroId);
+  const hero = getHeroesById(heroId)[0];
+
   if (!hero) {
     return <Redirect to="/" />;
   }
   const { id, superhero, publisher, alter_ego, first_appearance, characters } =
     hero;
 
+  const handleReturn = () => {
+    history.goBack();
+  };
+
   return (
-    <div>
-      <h1>HeroScreen</h1>
+    <div className="row mt-5">
+      <div className="col-4">
+        <img
+          className="w-100"
+          src={`../assets/heroes/${heroId}.jpg`}
+          alt={superhero}
+        />
+      </div>
+      <div className="col-8">
+        <h3>{superhero}</h3>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">Alter ego: {alter_ego}</li>
+          <li className="list-group-item">Publisher: {publisher}</li>
+          <li className="list-group-item">
+            First Appearance: {first_appearance}
+          </li>
+        </ul>
+        <h5>Characters</h5>
+        <p>{characters}</p>
+        <button className="btn btn-outline-success" onClick={handleReturn}>
+          Return
+        </button>
+      </div>
     </div>
   );
 };
