8d78029604ceb2111b1f9064442097703b62f452
ansango <anibalsantosgo@gmail.com>
2021-08-24 18:56:23 +0200

    validate route

--- src/components/heroes/HeroScreen.js
+++ src/components/heroes/HeroScreen.js
@@ -1,4 +1,14 @@
+import { Redirect, useParams } from "react-router-dom";
+import { getHeroesById } from "../../selectors/getHeroesById";
 export const HeroScreen = () => {
+  const { heroId } = useParams();
+  const hero = getHeroesById(heroId);
+  if (!hero) {
+    return <Redirect to="/" />;
+  }
+  const { id, superhero, publisher, alter_ego, first_appearance, characters } =
+    hero;
+
   return (
     <div>
       <h1>HeroScreen</h1>
