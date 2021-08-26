17902d4bef18289f3a8bf386485f0ab78c4916fe
ansango <anibalsantosgo@gmail.com>
2021-08-26 16:21:07 +0200

    private route

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,15 +1,23 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useAuth } from "../auth/AuthProvider";
 import { LoginScreen } from "../components/login/LoginScreen";
 
 import { DashboardRoutes } from "./DashboardRoutes";
+import { PrivateRoute } from "./PrivateRoute";
 
 export const AppRouter = () => {
+  const { user } = useAuth();
   return (
     <Router>
       <div>
         <Switch>
           <Route exact path="/login" component={LoginScreen} />
-          <Route path="/" component={DashboardRoutes} />
+
+          <PrivateRoute
+            path="/"
+            component={DashboardRoutes}
+            isAuth={user.logged}
+          />
         </Switch>
       </div>
     </Router>
